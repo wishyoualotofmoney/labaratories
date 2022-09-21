3cc2d20c802da4bf37726e2d9767f5e30083ed5e
wishyoualotofmoney <example.qzagaz@gmail.com>
2022-09-21 14:09:53 +0300

    вввв

--- halitovich_AC_21_04/halitovich_AC_21_04/main.cpp
+++ halitovich_AC_21_04/halitovich_AC_21_04/main.cpp
@@ -1,42 +1,11 @@
 ﻿#include <iostream>
 using namespace std;
 
-struct pipe {
-	float diametrPipe;
-	float lenghtPipe;
-	bool inRepair;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << "Hello World!\n" << a;
+    return 0;
+}
 
-	bool check() {
-		if (diametrPipe > 0 && lenghtPipe > 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-
-
-
-
-};
-
-struct station {
-	string name;
-	int workshop;
-	int activeWorkshop;
-	double efficiency;
-
-	bool check() {
-		if (name != "" && activeWorkshop > 0 && workshop > 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-
-
-
-};
